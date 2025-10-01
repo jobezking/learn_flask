7ac3ee367849072af1c3b255e7b63f600b1befdf
jobezking <52982865+jobezking@users.noreply.github.com>
2025-09-30 20:06:48 -0400

    update

--- flask_mega_tutorial/microblog/app/routes.py
+++ flask_mega_tutorial/microblog/app/routes.py
@@ -3,4 +3,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    page_title = {'text': 'Life Expectancy Calculator'}
+    return '''
+    <html>
+        <head>
+            <title>Home Page - Life Expectancy Calculator</title>
+        </head>
+        <body>
+            <h3>''' + page_title['text'] + '''</h3>
+        </body>
+    </html>'''
