5dc0d922b3ba31d901ae023dd287e7ebcfc4cb75
jobezking <52982865+jobezking@users.noreply.github.com>
2025-09-30 22:03:50 -0400

    Update

--- flask_mega_tutorial/microblog/app/routes.py
+++ flask_mega_tutorial/microblog/app/routes.py
@@ -5,4 +5,12 @@ from flask import render_template
 @app.route('/index')
 def index():
     page_title = {'text': 'Life Expectancy Calculator'}
-    return render_template('index.html', title='Home', page_title=page_title)
+    demographics = [
+        {'birth_year': 1970},
+        {'state': 'VA'}, 
+        {'county': 'Floyd'},
+        {'value': 30},
+        {'Race': 'White'},
+        {'Sex': 'Male'}  
+    ]
+    return render_template('index.html', title='Home', page_title=page_title, demographics=demographics)
